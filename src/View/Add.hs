6eb23c0d8a132ff4f1a190d3240109a056025cc4
Katie Miller <katie@codemiller.com>
2014-04-01 16:20:37 +1000

    Added some CSS and images

--- src/View/Add.hs
+++ src/View/Add.hs
@@ -2,7 +2,7 @@
 
 module View.Add (render) where
 
-import Text.Blaze.Html5.Attributes (action, method, name, type_)
+import Text.Blaze.Html5.Attributes (action, method, name, type_, class_)
 import Text.Blaze.Html.Renderer.Text
 import View.Header
 import qualified Data.Text.Lazy as D 
@@ -15,7 +15,7 @@ import qualified Text.Blaze.Html5 as H
 render :: D.Text
 render = renderHtml . H.docTypeHtml $ do
   header
-  H.body $ do
+  H.body H.! class_ "add" $ do
     H.h2 "Add a definition to the treasure chest"
     H.form H.! action "/add" H.! method "post" $ do
       H.p "Phrase: "
